c778a3efc6a823f362804487049745c12687a5f9
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2023-08-29 11:29:38 +0900

    [Bronze III] Title: 배수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    while(1){
+        int a;
+        scanf("%d", &a);
+        if(a == 0) break;
+        if(a % n != 0){
+            printf("%d is NOT a multiple of %d.\n", a, n);
+            continue;
+        }
+        else{
+            printf("%d is a multiple of %d.\n", a, n);
+            continue;
+        }
+    }
+    return 0;
+}
