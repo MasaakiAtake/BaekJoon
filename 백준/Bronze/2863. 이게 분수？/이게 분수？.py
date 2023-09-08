c914750799790c72a678ec476ca26c24afb4782e
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2023-09-08 10:38:16 +0900

    [Bronze III] Title: 이게 분수?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2863. 이게 분수？/이게 분수？.py
@@ -0,0 +1,18 @@
+def swap(l):
+    temp = l[0][0]
+    l[0][0] = l[1][0]
+    l[1][0] = l[1][1]
+    l[1][1] = l[0][1]
+    l[0][1] = temp
+    return l
+
+def cal(l):
+    return l[0][0] / l[1][0] + l[0][1] / l[1][1]
+
+
+l = [list(map(int, input().split())) for x in range(2)]
+result = [cal(l)]
+count = 0
+for i in range(3):
+    result.append(cal(swap(l)))
+print(result.index(max(result)))
