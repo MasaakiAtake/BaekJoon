4d180ddaa34e68c3a8bdc940bac52dfaf4119d50
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2023-09-12 09:57:10 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 472 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,11 @@
+nums = list(map(int,input().split()))
+n = 1
+while True:
+    cnt = 0
+    for i in nums:
+        if n%i==0:
+            cnt+=1
+    if cnt>=3:
+        print(n)
+        break
+    n+=1
