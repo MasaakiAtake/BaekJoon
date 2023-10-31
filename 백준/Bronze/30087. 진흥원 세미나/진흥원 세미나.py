2568d3fd3c17b22392461b41571e713bd797c07a
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2023-10-31 10:10:05 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,7 @@
+mydict = {"Algorithm":"204", "DataAnalysis":"207", "ArtificialIntelligence":"302", "CyberSecurity":"B101", "Network":"303", "Startup":"501", "TestStrategy":"105"}
+n = int(input())
+my = [input() for _ in range(n)]
+
+
+for my in my:
+    print(mydict[my])
