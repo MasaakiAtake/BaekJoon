97dfaef18ce6e48a39af31a0eebcaae8bbbd85cc
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2023-11-04 10:55:25 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+n = input()
+
+if n == "M":
+    print("MatKor")
+elif n == "W":
+    print("WiCys")
+elif n == "C":
+    print("CyKor")
+elif n == "A":
+    print("AlKor")
+elif n == "$":
+    print("$clear")
