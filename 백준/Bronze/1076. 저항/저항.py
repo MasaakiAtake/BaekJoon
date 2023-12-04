da514c2f3e27bc2abef40a6186dd686bc0927109
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2023-12-05 08:43:55 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+color = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+A = color.index(input())
+B = color.index(input())
+C = color.index(input())
+
+print(int(str(A)+str(B)) * (10** C))
