4b93fb73b7a8eba408076936164b49f109e7c6bc
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2024-02-06 10:27:22 +0900

    [Bronze III] Title: 사분면, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,19 @@
+n = int(input())
+q1, q2, q3, q4, axis = 0, 0, 0, 0, 0
+for i in range(n):
+    a, b = map(int,input().split())
+    if a > 0 and b > 0:
+        q1 += 1
+    elif a < 0 and b > 0:
+        q2 += 1
+    elif a < 0 and b < 0:
+        q3 += 1
+    elif a > 0 and b < 0:
+        q4 += 1
+    else:
+        axis += 1
+print('Q1:', q1)
+print('Q2:', q2)
+print('Q3:', q3)
+print('Q4:', q4)
+print('AXIS:', axis)
