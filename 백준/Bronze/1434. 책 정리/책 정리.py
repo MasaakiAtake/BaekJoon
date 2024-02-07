216ae4ae91e14de947383204011718165fff50da
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2024-02-07 15:11:27 +0900

    [Bronze II] Title: 책 정리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1434. 책 정리/책 정리.py
@@ -0,0 +1,11 @@
+n, m= map(int,input().split())
+box = list(map(int,input().split()))
+book = list(map(int,input().split()))
+
+for i in range(len(book)):
+    for j in range(len(box)):
+        if book[i] > box[j]:
+            continue
+        box[j] -= book[i]
+        break
+print(sum(box))
