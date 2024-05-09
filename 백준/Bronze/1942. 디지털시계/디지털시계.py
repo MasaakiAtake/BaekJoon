b6cbf04428cde4038d0bfa98ace680ae8d4773cc
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2024-05-09 18:08:35 +0900

    [Bronze II] Title: 디지털시계, Time: 712 ms, Memory: 38096 KB -BaekjoonHub

---
+++ 백준/Bronze/1942. 디지털시계/디지털시계.py
@@ -0,0 +1,37 @@
+import datetime
+from datetime import timedelta
+ 
+for i in range(3):
+    start,end=input().split()
+ 
+    st=int(start.replace(":",""))
+    ed=int(end.replace(":",""))
+ 
+    stH,stM,stS=map(int,start.split(":"))
+    edH,edM,edS=map(int,end.split(":"))
+ 
+    stime=datetime.datetime(2021,1,1,stH,stM,stS)
+ 
+    tmp_time=stime.strftime('%H%M%S')
+ 
+
+    sec=1
+ 
+    cnt=0
+    if int(tmp_time)%3==0:
+            cnt+=1
+    etime=datetime.datetime(2021,1,1,edH,edM,edS)
+    etime=etime.strftime('%H%M%S')
+ 
+    while True:
+        if tmp_time==etime:
+            break
+        
+      
+        stime=stime+timedelta(seconds=sec)
+        tmp_time=stime.strftime('%H%M%S')
+        if int(tmp_time)%3==0:
+            cnt+=1
+ 
+ 
+    print(cnt)
