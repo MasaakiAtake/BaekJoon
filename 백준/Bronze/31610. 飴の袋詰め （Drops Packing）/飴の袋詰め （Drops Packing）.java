837e84bebe54871a45c38a31039fb6ac1ac24a35
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2024-05-17 13:25:38 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int temp = (a*b)+c;
+        System.out.println(temp);
+    }
+}
