0987ea6203941b6003b95ebcb770daf253e640ba
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2024-06-04 08:07:16 +0900

    [Bronze III] Title: 개표, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        String str = sc.nextLine();
+        int count = 0;
+        
+        for (char result : str.toCharArray()){
+            if(result =='A'){
+                count++;
+            }
+        }
+        String ret = "Tie";
+        if(count * 2 > N) {
+            ret ="A";
+        } else if (count * 2 < N){
+            ret ="B";
+        }
+        System.out.println(ret);
+    }
+}
