ae5484c33acb40608fe44594874876a5dd3ffac6
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2024-12-02 09:31:20 +0900

    [Silver V] Title: 조약돌, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2097. 조약돌/조약돌.py
@@ -0,0 +1,23 @@
+def square_range(pebbles):
+    i = 0
+    while pow(i, 2) < pebbles:
+        i += 1
+    return i
+
+def min_circum(num, pebbles):
+    if (num-1) * num > pebbles:
+        return (num-2 + num-1) * 2
+    else:
+        return (num-1) * 4
+    
+if __name__ == "__main__":
+    pebbles = int(input())
+    
+    if pebbles == 0:
+        print(0)
+    elif pebbles == 1:
+        print(4)
+    else:
+        sq_num = square_range(pebbles)
+        circumference = min_circum(sq_num, pebbles)
+        print(circumference)
