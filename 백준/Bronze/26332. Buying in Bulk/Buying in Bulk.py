9ebd19cc3849a0c6f24a9ed77a36c9794888d761
MasaakiAtake <102898729+MasaakiAtake@users.noreply.github.com>
2025-02-20 17:12:38 +0900

    [Bronze IV] Title: Buying in Bulk, Time: 308 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/26332. Buying in Bulk/Buying in Bulk.py
@@ -0,0 +1,12 @@
+def sol(cnt, price):
+    if cnt>1:
+        return cnt*price-(cnt-1)*2
+    else:
+        return price
+ 
+if __name__ == '__main__':
+    n = int(input())
+    for _ in range(n):
+        cnt, price = map(int, input().split())
+        print(cnt, price)
+        print(sol(cnt, price))
